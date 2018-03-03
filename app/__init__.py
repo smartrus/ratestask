218906038ee2aa3508cbf4c8b0549169ddd5e6f8
Rustem Abdrakhmanov <rustemabd@gmail.com>
2018-03-03 13:44:41 +0300

    Wrote simple Get API without DB interaction as a POC. Unit tests are successful

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,37 @@
 # app/__init__.py
 
+# existing import remains
 from flask_api import FlaskAPI
 
 # local import
 from instance.config import app_config
 
+from flask import request, jsonify, abort
+
+    #####################
+    # existing code remains #
+    #####################
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    @app.route('/rates', methods=['GET'])
+    def rates():
+        if request.method == "GET":
+            # GET
+            results = []
+
+            obj = {
+                'orig_code': 'CNSGH',
+                'dest_code': 'EETLL',
+                'day': '2016-01-01',
+                'price': '1244'
+            }
+            results.append(obj)
+
+            response = jsonify(results)
+            response.status_code = 200
+            return response
 
     return app
--- test_rates.py
+++ test_rates.py
@@ -13,6 +13,7 @@ class RatesTestCase(unittest.TestCase):
         self.client = self.app.test_client
 
     # my smoke test
+    # tries to get all the prices and checks if CNSGH is in the returned data
     def test_api_can_get_prices(self):
         """Test API can get a single rate by using it's id."""
         result = self.client().get(
@@ -20,12 +21,15 @@ class RatesTestCase(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
         self.assertIn('CNSGH', str(result.data))
 
+    # a get request from the task that should be executed successfully
     def test_api_can_get_rates_by_id(self):
         """Test API can get a single rate by using it's id."""
         result = self.client().get(
             'rates?date_from=2016-01-01&date_to=2016-01-10&origin=CNSGH&destination=north_europe_main')
         self.assertEqual(result.status_code, 200)
         self.assertIn('CNSGH', str(result.data))
+
+    # TODO: a post request from the task
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
